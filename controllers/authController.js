8c1294559c85d7f1a762ef55fee0b400968adf38
M Rifqi <muhamadrifqi.id@gmail.com>
2025-06-18 10:18:29 +0700

    hash password user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,18 @@
 const { User } = require('../models');
+const { hashPassword } = require('../utils')
 
 const signup = async (req, res, next) => {
-    const { name, email, password, role } = req.body
     try {
-        await User.create({ name, email, password, role })
+        const { name, email, password, role } = req.body
+        const isEmailExist = await User.findOne({ email })
+        if (isEmailExist) {
+            res.code = 400
+            throw new Error('Email already exist')
+        }
+
+        const hashedPassword = await hashPassword(password)
+
+        await User.create({ name, email, password: hashedPassword, role })
         res.status(201).json({ code: 201, status: true, message: "user register successfully!" })
     } catch (error) {
         next(error)
